1e998c232aa6ada654a73470afac5f9ab30e8b26
~servidor <~engaero.jns@gmail.com~>
2022-11-10 12:40:14 -0600

    Melhorias

--- src/page/Apoiadores.jsx
+++ src/page/Apoiadores.jsx
@@ -7,6 +7,7 @@ export default ()=>{
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius reprehenderit voluptates incidunt fugiat laudantium dolorem assumenda veniam officiis quasi vitae, dolor et doloribus, nisi voluptate eos minima consectetur placeat error!
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius reprehenderit voluptates incidunt fugiat laudantium dolorem assumenda veniam officiis quasi vitae, dolor et doloribus, nisi voluptate eos minima consectetur placeat error!
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, harum minus aliquid in sint, assumenda debitis hic dolorem sapiente velit rem, consectetur molestias. Adipisci minima incidunt consequatur, tenetur at deleniti.
             </p>
         </>
     )
