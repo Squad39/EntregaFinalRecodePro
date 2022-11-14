f251a2d78687bb51d4151b3b706c0c8294eb8f80
engaero.jns@gmail.com <107485917+JansRodriguez@users.noreply.github.com>
2022-11-14 17:53:50 -0300

    Atualizando cards

--- frontendalpharural/src/Card/Cards.jsx
+++ frontendalpharural/src/Card/Cards.jsx
@@ -1,9 +1,11 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+import './Cards.css';
+
 export default () => {
     return (
-        <section>
+        <section className="Cards">
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src="holder.js/100px180" />
                 <Card.Body>
