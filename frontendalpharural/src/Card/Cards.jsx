eac668868d1428e04e350ab8cdb104bb6b511528
Janes Rodrigues <engaero.jns@gmail.com>
2022-11-15 01:41:14 -0300

    Atualizando a pagina NÓS, HOME e os CARDS

--- frontendalpharural/src/Card/Cards.jsx
+++ frontendalpharural/src/Card/Cards.jsx
@@ -6,36 +6,35 @@ import './Cards.css';
 export default () => {
     return (
         <section className="Cards">
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+            <Card style={{ width: '18rem', textAlign: 'center' }}>
+                <Card.Img variant="top" src="imagens/1-recode.png" />
                 <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                    <Card.Title>Depoimentos</Card.Title>
+                    <Card.Text style={{fontSize: '12px', textAlign: 'justify'}}>
+                    “Tem sido um prazer poder colaborar com o projeto, ter status de como é aplicado o recurso que doamos e ver o andamento e atendimento desde alunos. A educação realmente transforma. Feliz de participar dessa mudança de vidas.”
+                    <p>Mário Cabral</p>
                     </Card.Text>
                 </Card.Body>
             </Card>
 
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+            <Card style={{ width: '18rem', textAlign: 'center'  }}>
+                <Card.Img variant="top" src="imagens/2-recode.png" />
                 <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                    <Card.Title>Depoimentos</Card.Title>
+                    <Card.Text style={{fontSize: '12px', textAlign: 'justify'}}>
+                    “Aplicar aquilo que aprendemos na faculdade e já poder mudar o mundo, mesmo que seja somente um passo de cada, é algo incrível. Poder participar com uma parte das férias e ver o sorriso desse jovens e adultos, é algo que faz a gente se sentir realizados.”
+                    <p>Adriana Magalhães</p>
                     </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
+                    <Button variant="primary">Participe Você</Button>
                 </Card.Body>
             </Card>
 
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+            <Card style={{ width: '18rem', textAlign: 'center'  }}>
+                <Card.Img variant="top" src="imagens/3-recode.png" />
                 <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                    <Card.Title>Depoimentos</Card.Title>
+                    <Card.Text style={{fontSize: '12px', textAlign: 'justify'}}>
+                    “Ouvir do projeto da Alpha Rural e decidir parte doando livros e materiais escolares, assim como poder vim até essa comunidade e doar uma parte das férias. É algo surreal ver suas ações modificando a perspectivas das pessoas. Usar o privilégio que temos para dar essa mesma perspectiva a outros.”
                     </Card.Text>
                 </Card.Body>
             </Card>
--- frontendalpharural/src/page/Nos.jsx
+++ frontendalpharural/src/page/Nos.jsx
@@ -1,13 +1,16 @@
 export default ()=>{
     return(
-        <>
-            <h1>Quem na verdade somos</h1>
-            <p>Conteudo Nós</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius reprehenderit voluptates incidunt fugiat laudantium dolorem assumenda veniam officiis quasi vitae, dolor et doloribus, nisi voluptate eos minima consectetur placeat error!
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius reprehenderit voluptates incidunt fugiat laudantium dolorem assumenda veniam officiis quasi vitae, dolor et doloribus, nisi voluptate eos minima consectetur placeat error!
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius reprehenderit voluptates incidunt fugiat laudantium dolorem assumenda veniam officiis quasi vitae, dolor et doloribus, nisi voluptate eos minima consectetur placeat error!
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+        <section className="" style={{padding: '30px 100px', fontSize: '22px', textAlign: 'justify', fontWeight: '500'}}>
+            <p>
+                Somos jovens que desejam transformar a realidade de pessoas menos favorecidas. O projeto foi desenvolvido visando tornar o mundo um lugar melhor. Jovens que entendem que a educação, é a porta para toda e qualquer realização pessoal. Entendemos que a educação pode mudar tudo ao redor e proporcinar soluções ambientais, sociais, culturais, tecnológicas e financeiras.
             </p>
-        </>
+            <p>
+                Por isso queremos maximimar nosso potencial, para entregar o melhor. Para que o sonho, de fazer do mundo melhor acontecer. Entendendo que toda mundança começa de algo pequeno, para tomar proporções gigantescas. Assim, como a teoria do Caos, traz a ideia de que pequenos atos podem alterar grandes ciclos, nos propormos a ser esse bater de asas.
+            </p>
+            <p>   
+                Nessa visão, queremos contribuir para que todos os jovens e adultos sejam alfabetizados e tenham conhecimento básico em ciências linguísticas, ciências tecnológicas e todas as áreas da educação nas regiões rurais e mais afastadas, impactando a sociedade da região como um todo.
+            </p>
+            
+        </section>
     )
 }
