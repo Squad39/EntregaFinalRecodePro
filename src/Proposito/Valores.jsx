ee790466ce49dffaaaf4f4f0aac448b7624bc5ad
~servidor <~engaero.jns@gmail.com~>
2022-11-16 06:25:57 -0600

    Tratando componentes Valores.jsx . css

--- src/Proposito/Valores.jsx
+++ src/Proposito/Valores.jsx
@@ -3,23 +3,23 @@ import './Proposito.css';
 export default ()=>{
     return(
         <section className='valores'>
-            <h2>NOSSA MISSÃO</h2>
+            <h2><strong>NOSSA MISSÃO</strong></h2>
             <p>Impactar a sociedade rural como um todo. Crianças, jovens e adultos. Mudando a realidade local e contribuido para erradicar o analfabetismo. Levar ao maior número de pessoas possíveis, o crescimento constante das viabilidades através da tecnologia e uso de ferramentas computacionais. 
             </p>
-            <h2>NOSSA VISÃO</h2>
+            <h2><strong>NOSSA VISÃO</strong></h2>
             <p>Uma educação equitativa e igualitária para todas as crianças, jovens e adultos de regiões rurais do país. Tornar a sociedade rural um grande polo de empreendorismo, trazer conhecimento técnico em diversas áreas, habilidades científicas através da educação e acesso a leitura.
             </p>
-            <h2>NOSSOS VALORES</h2>
-            <p>UFoco em pessoas. Ética e Sustentabilidade. Transferência. Imparcialidade.</p>
+            <h2><strong>NOSSOS VALORES</strong></h2>
+            <p>Foco em pessoas. Ética e Sustentabilidade. Transferência. Imparcialidade.</p>
+            <hr/>
+            <p><em><strong>Quem faz:</strong></em> Parceira entre instituições e empresas privadas.</p>
+            <p><em><strong>Para quem:</strong></em> Para Crianças, jovens e adultos com dificuldade de aprendizagem de escrita e leitura.</p>
+            <p><em><strong>Quem participa:</strong></em> Crianças, jovens e adultos.</p>
+            <p><em><strong>O que:</strong></em> Um site que ajude as empresas e intuições a se cadastrarem, doarem e interagirem.</p>
+            <p><em><strong>Como:</strong></em> Através de mobilização de voluntário ao local alvo e em segunda etapa, aulas EAD, máquinas e tecnologia para áreas rurais.</p>
+            <p><em><strong>Porque:</strong></em> Para contribuir para a diminuição do analfabetismo em áreas rurais. Impactando toda a região, e o desenvolvimento socioeconômico.</p>
             <hr />
-            Quem faz: Parceira entre instituições e empresas privadas.
-            Para quem: Para Crianças, jovens e adultos com dificuldade de aprendizagem de escrita e leitura.
-            Quem participa: Crianças, jovens e adultos.
-            O que: Um site que ajude as empresas e intuições a se cadastrarem, doarem e interagirem.
-            Como: Através de mobilização de voluntário ao local alvo e em segunda etapa, aulas EAD, máquinas e tecnologia para áreas rurais.
-            Porque: Para contribuir para a diminuição do analfabetismo em áreas rurais. Impactando toda a região, e o desenvolvimento socioeconômico.
-
-            Participe! Todos temos direito a uma educação igualitária e inclusiva.
+            <h3><strong>Participe! Todos temos direito a uma educação igualitária e inclusiva.</strong></h3>
             
         </section>
     )
