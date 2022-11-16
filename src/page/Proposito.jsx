4fc51b8ffdd5f159a17da835b327ce7d6e956ef9
~servidor <~engaero.jns@gmail.com~>
2022-11-16 07:01:41 -0600

    Alterações

--- src/page/Proposito.jsx
+++ src/page/Proposito.jsx
@@ -2,7 +2,7 @@ import Valores from "../Proposito/Valores"
 
 export default ()=>{
     return(
-        <main className="Proposito">
+        <main className="Proposito" style={{backgroundImage: 'linear-gradient(to top, rgba(59, 205, 210, 0.604), rgba(17, 55, 130, 0.664))'}}>
             <Valores/>
         </main>
     )
