f29640f8edd0a37e7f6497f6e7d6fe06c32a0f7d
Janes Rodrigues <engaero.jns@gmail.com>
2022-11-16 00:02:05 -0300

    Atualizando página de proposito

--- src/Proposito/Valores.jsx
+++ src/Proposito/Valores.jsx
@@ -9,6 +9,18 @@ export default ()=>{
             <h2>NOSSA VISÃO</h2>
             <p>Uma educação equitativa e igualitária para todas as crianças, jovens e adultos de regiões rurais do país. Tornar a sociedade rural um grande polo de empreendorismo, trazer conhecimento técnico em diversas áreas, habilidades científicas através da educação e acesso a leitura.
             </p>
+            <h2>NOSSOS VALORES</h2>
+            <p>UFoco em pessoas. Ética e Sustentabilidade. Transferência. Imparcialidade.</p>
+            <hr />
+            Quem faz: Parceira entre instituições e empresas privadas.
+            Para quem: Para Crianças, jovens e adultos com dificuldade de aprendizagem de escrita e leitura.
+            Quem participa: Crianças, jovens e adultos.
+            O que: Um site que ajude as empresas e intuições a se cadastrarem, doarem e interagirem.
+            Como: Através de mobilização de voluntário ao local alvo e em segunda etapa, aulas EAD, máquinas e tecnologia para áreas rurais.
+            Porque: Para contribuir para a diminuição do analfabetismo em áreas rurais. Impactando toda a região, e o desenvolvimento socioeconômico.
+
+            Participe! Todos temos direito a uma educação igualitária e inclusiva.
+            
         </section>
     )
 }
